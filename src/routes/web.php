d628998ae6a2e92032a0ecf7384b0b3653845aae
Arvid de Jong <arvid@darvis.nl>
2023-02-21 16:42:01 +0100

    Route update

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,6 +6,6 @@ use Manta\LaravelUploads\Http\Livewire\Uploads\UploadsUpdate;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/pagina', UploadsList::class)->name('manta.uploads.list');
-Route::get('/pagina/toevoegen', UploadsCreate::class)->name('manta.uploads.create');
-Route::get('/pagina/aanpassen/{input}', UploadsUpdate::class)->name('manta.uploads.update');
+Route::get('/uploads', UploadsList::class)->name('manta.uploads.list');
+Route::get('/uploads/toevoegen', UploadsCreate::class)->name('manta.uploads.create');
+Route::get('/uploads/aanpassen/{input}', UploadsUpdate::class)->name('manta.uploads.update');
